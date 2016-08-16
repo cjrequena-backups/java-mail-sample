0b8463aae15fe49254ed261359a40ac699f74c18
Carlos José Requena Jiménez <carlosjose.requena@gmail.com>
2016-08-16 22:28:59 +0200

    First commit

---
+++ src/main/java/com/sample/JavaMailUtils.java
@@ -0,0 +1,518 @@
+package com.sample;
+
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.search.FlagTerm;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.Properties;
+
+/**
+ * Created by cjrequena on 16/08/16.
+ */
+
+/**
+ * See {@linktourl https://javamail.java.net/nonav/docs/api/}
+ * See {@linktourl https://javamail.java.net/nonav/docs/api/com/sun/mail/imap/package-summary.html}
+ * See {@linktourl https://javamail.java.net/nonav/docs/api/com/sun/mail/smtp/package-summary.html}
+ */
+@Data
+@Log4j2
+public class JavaMailUtils {
+
+    private JavaMailUtils() {
+
+    }
+
+    /**
+     * @param email
+     * @param password
+     * @param properties
+     * @return
+     */
+    public static Session getSession(final String email, final String password, Properties properties) {
+        Authenticator authentication = new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(email, password);
+            }
+        };
+        return Session.getInstance(properties, authentication);
+    }
+
+    /**
+     * @param folder
+     * @param fetchProfilesItems
+     * @return
+     * @throws MessagingException
+     */
+    public static Message[] getAllMessagesFromFolder(Folder folder, FetchProfile.Item... fetchProfilesItems) throws MessagingException {
+        try {
+            Message messages[] = folder.getMessages();
+            FetchProfile fp = new FetchProfile();
+            for (FetchProfile.Item fetchProfilesItem : fetchProfilesItems) {
+                fp.add(fetchProfilesItem);
+            }
+            folder.fetch(messages, fp);
+            return messages;
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    /**
+     * @param folder
+     * @param fetchProfilesItems
+     * @return
+     * @throws MessagingException
+     */
+    public static Message[] getSeenMessagesFromFolder(Folder folder, FetchProfile.Item... fetchProfilesItems) throws MessagingException {
+        try {
+            Message messages[] = folder.search(new FlagTerm(new Flags(Flags.Flag.SEEN), Boolean.TRUE));
+            FetchProfile fp = new FetchProfile();
+            for (FetchProfile.Item fetchProfilesItem : fetchProfilesItems) {
+                fp.add(fetchProfilesItem);
+            }
+            folder.fetch(messages, fp);
+            return messages;
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+
+    /**
+     * @param folder
+     * @param fetchProfilesItems
+     * @return
+     * @throws MessagingException
+     */
+    public static Message[] getNotSeenMessagesFromFolder(Folder folder, FetchProfile.Item... fetchProfilesItems) throws MessagingException {
+        try {
+            Message messages[] = folder.search(new FlagTerm(new Flags(Flags.Flag.SEEN), Boolean.FALSE));
+            FetchProfile fp = new FetchProfile();
+            for (FetchProfile.Item fetchProfilesItem : fetchProfilesItems) {
+                fp.add(fetchProfilesItem);
+            }
+            folder.fetch(messages, fp);
+            return messages;
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    /**
+     * @param folder
+     * @param fetchProfilesItems
+     * @return
+     * @throws MessagingException
+     */
+    public static Message[] getDraftMessagesFromFolder(Folder folder, FetchProfile.Item... fetchProfilesItems) throws MessagingException {
+        try {
+            Message messages[] = folder.search(new FlagTerm(new Flags(Flags.Flag.DRAFT), Boolean.TRUE));
+            FetchProfile fp = new FetchProfile();
+            for (FetchProfile.Item fetchProfilesItem : fetchProfilesItems) {
+                fp.add(fetchProfilesItem);
+            }
+            folder.fetch(messages, fp);
+            return messages;
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    /**
+     * @param folder
+     * @param fetchProfilesItems
+     * @return
+     * @throws MessagingException
+     */
+    public static Message[] getNotDraftMessagesFromFolder(Folder folder, FetchProfile.Item... fetchProfilesItems) throws MessagingException {
+        try {
+            Message messages[] = folder.search(new FlagTerm(new Flags(Flags.Flag.DRAFT), Boolean.FALSE));
+            FetchProfile fp = new FetchProfile();
+            for (FetchProfile.Item fetchProfilesItem : fetchProfilesItems) {
+                fp.add(fetchProfilesItem);
+            }
+            folder.fetch(messages, fp);
+            return messages;
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    /**
+     * Utility method to send simple HTML email
+     *
+     * @param session
+     * @param toEmail
+     * @param subject
+     * @param body
+     */
+    public static void sendEmail(Session session, String fromEmail, String toEmail, String replayToEmail, String subject, String body) {
+        try {
+            MimeMessage msg = new MimeMessage(session);
+            //set message headers
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+            msg.setFrom(new InternetAddress(fromEmail, "NoReply-JD"));
+
+            msg.setReplyTo(InternetAddress.parse(replayToEmail, false));
+
+            msg.setSubject(subject, "UTF-8");
+
+            msg.setText(body, "UTF-8");
+
+            msg.setSentDate(new Date());
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+            System.out.println("Message is ready");
+            Transport.send(msg);
+
+            log.debug("EMail Sent Successfully!!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Utility method to send email with attachment
+     * @param session
+     * @param toEmail
+     * @param subject
+     * @param body
+     */
+    public static void sendAttachmentEmail(Session session, String fromEmail, String toEmail, String replayToEmail, String subject, String body, FileDataSource fileDataSource){
+        try{
+            MimeMessage msg = new MimeMessage(session);
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+            msg.setFrom(new InternetAddress(fromEmail, "NoReply-JD"));
+
+            msg.setReplyTo(InternetAddress.parse(replayToEmail, false));
+
+            msg.setSubject(subject, "UTF-8");
+
+            msg.setSentDate(new Date());
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+
+            // Create the message body part
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            // Fill the message
+            messageBodyPart.setText(body);
+
+            // Create a multipart message for attachment
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Second part is attachment
+            messageBodyPart = new MimeBodyPart();
+
+            messageBodyPart.setDataHandler(new DataHandler(fileDataSource));
+            messageBodyPart.setFileName(fileDataSource.getName());
+            multipart.addBodyPart(messageBodyPart);
+
+            // Send the complete message parts
+            msg.setContent(multipart);
+
+            // Send message
+            Transport.send(msg);
+            log.debug("EMail Sent Successfully with attachment!!");
+        }catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Utility method to send image in email body
+     * @param session
+     * @param toEmail
+     * @param subject
+     * @param body
+     */
+    public static void sendImageEmail(Session session,  String fromEmail, String toEmail, String replayToEmail, String subject, String body, FileDataSource fileDataSource){
+        try{
+            MimeMessage msg = new MimeMessage(session);
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+            msg.setFrom(new InternetAddress(fromEmail, "NoReply-JD"));
+
+            msg.setReplyTo(InternetAddress.parse(replayToEmail, false));
+
+            msg.setSubject(subject, "UTF-8");
+
+            msg.setSentDate(new Date());
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+
+            // Create the message body part
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            messageBodyPart.setText(body);
+
+            // Create a multipart message for attachment
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Second part is image attachment
+            messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setDataHandler(new DataHandler(fileDataSource));
+            messageBodyPart.setFileName(fileDataSource.getName());
+            //Trick is to add the content-id header here
+            messageBodyPart.setHeader("Content-ID", "image_id");
+            multipart.addBodyPart(messageBodyPart);
+
+            //third part for displaying image in the email body
+            messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent("<h1>Attached Image</h1>" + "<img src='cid:image_id'>", "text/html");
+            multipart.addBodyPart(messageBodyPart);
+
+            //Set the multipart message to the email message
+            msg.setContent(multipart);
+
+            // Send message
+            Transport.send(msg);
+            log.debug("EMail Sent Successfully with image!!");
+        }catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    /**
+     * @param folder
+     * @param fetchProfilesItems
+     * @return
+     * @throws MessagingException
+     */
+    public static Message[] getAnsweredMessagesFromFolder(Folder folder, FetchProfile.Item... fetchProfilesItems) throws MessagingException {
+        try {
+            Message messages[] = folder.search(new FlagTerm(new Flags(Flags.Flag.ANSWERED), Boolean.TRUE));
+            FetchProfile fp = new FetchProfile();
+            for (FetchProfile.Item fetchProfilesItem : fetchProfilesItems) {
+                fp.add(fetchProfilesItem);
+            }
+            folder.fetch(messages, fp);
+            return messages;
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    /**
+     * @param folder
+     * @param fetchProfilesItems
+     * @return
+     * @throws MessagingException
+     */
+    public static Message[] getNotAnsweredMessagesFromFolder(Folder folder, FetchProfile.Item... fetchProfilesItems) throws MessagingException {
+        try {
+            Message messages[] = folder.search(new FlagTerm(new Flags(Flags.Flag.ANSWERED), Boolean.FALSE));
+            FetchProfile fp = new FetchProfile();
+            for (FetchProfile.Item fetchProfilesItem : fetchProfilesItems) {
+                fp.add(fetchProfilesItem);
+            }
+            folder.fetch(messages, fp);
+            return messages;
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    /**
+     * @param folder
+     * @param fetchProfilesItems
+     * @return
+     * @throws MessagingException
+     */
+    public static Message[] getFlaggedMessagesFromFolder(Folder folder, FetchProfile.Item... fetchProfilesItems) throws MessagingException {
+        try {
+            Message messages[] = folder.search(new FlagTerm(new Flags(Flags.Flag.FLAGGED), Boolean.TRUE));
+            FetchProfile fp = new FetchProfile();
+            for (FetchProfile.Item fetchProfilesItem : fetchProfilesItems) {
+                fp.add(fetchProfilesItem);
+            }
+            folder.fetch(messages, fp);
+            return messages;
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    /**
+     * @param folder
+     * @param fetchProfilesItems
+     * @return
+     * @throws MessagingException
+     */
+    public static Message[] getNotFlaggedMessagesFromFolder(Folder folder, FetchProfile.Item... fetchProfilesItems) throws MessagingException {
+        try {
+            Message messages[] = folder.search(new FlagTerm(new Flags(Flags.Flag.FLAGGED), Boolean.FALSE));
+            FetchProfile fp = new FetchProfile();
+            for (FetchProfile.Item fetchProfilesItem : fetchProfilesItems) {
+                fp.add(fetchProfilesItem);
+            }
+            folder.fetch(messages, fp);
+            return messages;
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    /**
+     * @param folder
+     * @param fetchProfilesItems
+     * @return
+     * @throws MessagingException
+     */
+    public static Message[] getRecentMessagesFromFolder(Folder folder, FetchProfile.Item... fetchProfilesItems) throws MessagingException {
+        try {
+            Message messages[] = folder.search(new FlagTerm(new Flags(Flags.Flag.RECENT), Boolean.TRUE));
+            FetchProfile fp = new FetchProfile();
+            for (FetchProfile.Item fetchProfilesItem : fetchProfilesItems) {
+                fp.add(fetchProfilesItem);
+            }
+            folder.fetch(messages, fp);
+            return messages;
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    /**
+     * @param folder
+     * @param fetchProfilesItems
+     * @return
+     * @throws MessagingException
+     */
+    public static Message[] getNotRecentFlaggedMessagesFromFolder(Folder folder, FetchProfile.Item... fetchProfilesItems) throws MessagingException {
+        try {
+            Message messages[] = folder.search(new FlagTerm(new Flags(Flags.Flag.RECENT), Boolean.FALSE));
+            FetchProfile fp = new FetchProfile();
+            for (FetchProfile.Item fetchProfilesItem : fetchProfilesItems) {
+                fp.add(fetchProfilesItem);
+            }
+            folder.fetch(messages, fp);
+            return messages;
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    /**
+     * @param folder
+     * @param fetchProfilesItems
+     * @return
+     * @throws MessagingException
+     */
+    public static Message[] getDeletedMessagesFromFolder(Folder folder, FetchProfile.Item... fetchProfilesItems) throws MessagingException {
+        try {
+            Message messages[] = folder.search(new FlagTerm(new Flags(Flags.Flag.DELETED), Boolean.TRUE));
+            FetchProfile fp = new FetchProfile();
+            for (FetchProfile.Item fetchProfilesItem : fetchProfilesItems) {
+                fp.add(fetchProfilesItem);
+            }
+            folder.fetch(messages, fp);
+            return messages;
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    /**
+     * @param folder
+     * @param fetchProfilesItems
+     * @return
+     * @throws MessagingException
+     */
+    public static Message[] getNotDeletedFlaggedMessagesFromFolder(Folder folder, FetchProfile.Item... fetchProfilesItems) throws MessagingException {
+        try {
+            Message messages[] = folder.search(new FlagTerm(new Flags(Flags.Flag.DELETED), Boolean.FALSE));
+            FetchProfile fp = new FetchProfile();
+            for (FetchProfile.Item fetchProfilesItem : fetchProfilesItems) {
+                fp.add(fetchProfilesItem);
+            }
+            folder.fetch(messages, fp);
+            return messages;
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+
+    /**
+     * @param fromFolder
+     * @param destinationFolder
+     * @param messages
+     * @throws MessagingException
+     */
+    public static void copyMessages(Folder fromFolder, Folder destinationFolder, Message... messages) throws MessagingException {
+        try {
+            fromFolder.copyMessages(messages, destinationFolder);
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    /**
+     * @param fromFolder
+     * @param destinationFolder
+     * @param messages
+     * @throws MessagingException
+     */
+    public static void moveMessages(Folder fromFolder, Folder destinationFolder, Message... messages) throws MessagingException {
+        try {
+            copyMessages(fromFolder, destinationFolder, messages);
+            for (Message message : messages) {
+                message.setFlag(Flags.Flag.DELETED, Boolean.TRUE);
+            }
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+
+
+
+}
+
+
---
+++ src/test/java/com/sample/JavaMailUtilsTest.java
@@ -0,0 +1,124 @@
+package com.sample;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.Test;
+
+import javax.mail.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+/**
+ * Created by cjrequena on 16/08/16.
+ */
+@Log4j2
+public class JavaMailUtilsTest {
+
+    Folder inbox;
+    Folder processed;
+    String email="carlosjose.requena@outlook.com";
+    String password ="$$@2wkAwsswY#4y";
+
+    @Test
+    public void JavaMailUtilsTest() {
+        try {
+
+            // SEND AN EMAIL
+            Properties properties = System.getProperties();
+            properties.put("mail.smtp.user", email);
+            properties.put("mail.smtp.host", "smtp-mail.outlook.com");
+            properties.put("mail.smtp.port", "587");
+            properties.put("mail.smtp.starttls.enable", "true");
+            properties.put("mail.smtp.auth", "true");
+            properties.put("mail.smtp.socketFactory.port", "587");
+            properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            properties.put("mail.smtp.socketFactory.fallback", "true");
+            Session session = JavaMailUtils.getSession(email, password, properties);
+            JavaMailUtils.sendEmail(session, "no-replay@gmail.com", email, "no-replay@gmail.com", "TEST", "UN BODY TEST");
+
+
+            final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+            // Set manual Properties
+            properties.setProperty("mail.imaps.socketFactory.class", SSL_FACTORY);
+            properties.setProperty("mail.imaps.socketFactory.fallback", "false");
+            properties.setProperty("mail.imaps.port", "993");
+            properties.setProperty("mail.imaps.socketFactory.port", "993");
+            properties.put("mail.imaps.host", "imap-mail.outlook.com");
+            /* Create the session  */
+            session = JavaMailUtils.getSession(email, password, properties);
+            Store store = session.getStore("imaps");
+            store.connect();
+
+            /* Open the inbox using store. */
+            inbox = store.getFolder("INBOX");
+            inbox.open(Folder.READ_WRITE);
+
+            Message messages[] = JavaMailUtils.getAllMessagesFromFolder(inbox, FetchProfile.Item.ENVELOPE, FetchProfile.Item.CONTENT_INFO, FetchProfile.Item.FLAGS, FetchProfile.Item.SIZE);
+            //Message messages[] = JavaMailUtils.getSeenMessagesFromFolder(inbox, FetchProfile.Item.ENVELOPE, FetchProfile.Item.CONTENT_INFO, FetchProfile.Item.FLAGS, FetchProfile.Item.SIZE);
+            log.debug("No. of Unread Messages : " + inbox.getUnreadMessageCount());
+
+            /* Open the inbox using store. */
+            processed = store.getFolder("PROCESSED");
+            processed.open(Folder.READ_WRITE);
+
+            /*Copy messges from a folder to another folder*/
+            JavaMailUtils.moveMessages(inbox, processed, messages);
+
+            try {
+
+                printAllMessages(messages);
+
+                inbox.close(true);
+                store.close();
+
+            } catch (Exception ex) {
+                log.debug("Exception arise at the time of read mail");
+                ex.printStackTrace();
+            }
+
+        } catch (MessagingException e) {
+            log.debug("Exception while connecting to server: " + e.getLocalizedMessage());
+            e.printStackTrace();
+            System.exit(2);
+        }
+
+
+    }
+
+    public void printAllMessages(Message[] msgs) throws Exception {
+        for (int i = 0; i < msgs.length; i++) {
+            log.debug("MESSAGE #" + (i + 1) + ":");
+            printEnvelope(msgs[i]);
+        }
+    }
+
+    public void printEnvelope(Message message) throws Exception {
+
+        Address[] a;
+
+        // FROM
+        if ((a = message.getFrom()) != null) {
+            for (int j = 0; j < a.length; j++) {
+                log.debug("De : " + a[j].toString());
+            }
+        }
+
+        String subject = message.getSubject();
+
+        Date receivedDate = message.getReceivedDate();
+        Date sentDate = message.getSentDate(); // receivedDate is returning
+        // null. So used getSentDate()
+
+        //Dar Formato a la fecha
+        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+        log.debug("Asunto : " + subject);
+
+        if (receivedDate != null) {
+            log.debug("Recibido: " + df.format(receivedDate));
+        }
+
+        log.debug("Enviado : " + df.format(sentDate));
+    }
+
+}
